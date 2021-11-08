1562d9db3533b54160fd4dcf1d6916888be0a994
Jay Bittner <jtbitt@gmail.com>
2021-11-08 18:03:09 -0500

    Set up error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,32 @@
+import { StrictMode, Suspense } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { CssBaseline } from '@material-ui/core';
+
+// import { store } from '@store';
+// import { LoadingIndicator } from '@components';
+
+import { ErrorBoundary } from './ErrorBoundary';
+// import { Layout } from './Layout';
+// import { Routes } from './Routes';
+
 export const App = () => {
-  return <div>Hello new boilerplate</div>;
+  return (
+    <StrictMode>
+      {/* <Provider store={store}> */}
+      {/* <Router> */}
+      {/* <CssBaseline> */}
+      {/* <Layout> */}
+      <ErrorBoundary>
+        {/* <Suspense fallback={<LoadingIndicator />}> */}
+        {/* <Routes /> */}
+        <div>Hello new boilerplate</div>
+        {/* </Suspense> */}
+      </ErrorBoundary>
+      {/* </Layout> */}
+      {/* </CssBaseline> */}
+      {/* </Router> */}
+      {/* </Provider> */}
+    </StrictMode>
+  );
 };
---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,58 @@
+import { Component, ReactNode, ErrorInfo } from 'react';
+
+// import { Heading, Typography, Button } from '@components';
+
+interface IErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface IErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<
+  IErrorBoundaryProps,
+  IErrorBoundaryState
+> {
+  public state: IErrorBoundaryState = {
+    hasError: false,
+  };
+
+  private handleRefreshClick = () => {
+    window.location.reload();
+  };
+
+  public static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error: ', error, errorInfo);
+  }
+
+  public render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return (
+        <>
+          {/* <Heading>Woops 🙈</Heading>
+          <Typography align="center" variant="body2" gutterBottom>
+            Something went wrong on our end, the below button is not AZ-5, what
+            could go wrong...
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleRefreshClick}
+          >
+            Refresh
+          </Button> */}
+        </>
+      );
+    }
+
+    return children;
+  }
+}
