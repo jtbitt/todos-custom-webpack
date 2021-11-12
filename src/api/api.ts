642acbc571d80acca6b83c6a07af2d07fc11a9b2
Jay Bittner <jtbitt@gmail.com>
2021-11-12 14:58:20 -0500

    Set up api

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+export interface ITodosData {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export const fetchTodosData = async (): Promise<ITodosData> => {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/todos',
+  );
+  const data = await response.json();
+
+  return data;
+};
